e9c4e5a51885b762a119e8a66bea530b5c0450f5
Arthur Oliveira <144960267+Arthurolv@users.noreply.github.com>
2024-11-11 20:59:58 -0300

    Update BrincosActivity.kt

--- app/src/main/java/com/example/ljoias/BrincosActivity.kt
+++ app/src/main/java/com/example/ljoias/BrincosActivity.kt
@@ -1,4 +1,12 @@
 package com.example.ljoias
 
-class BrincosActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PulseirasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brincos) // Certifique-se de que você tenha criado o layout activity_pulseiras.xml
+    }
 }
