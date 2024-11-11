2f650773a3e70539126ecbf007c86a4bf3028cff
Arthur Oliveira <144960267+Arthurolv@users.noreply.github.com>
2024-11-11 20:59:30 -0300

    Update AneisActivity.kt

--- app/src/main/java/com/example/ljoias/AneisActivity.kt
+++ app/src/main/java/com/example/ljoias/AneisActivity.kt
@@ -1,4 +1,16 @@
 package com.example.ljoias
 
 class AneisActivity {
+  package com.example.ljoias
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PulseirasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_Aneis) // Certifique-se de que você tenha criado o layout activity_pulseiras.xml
+    }
+}
 }
